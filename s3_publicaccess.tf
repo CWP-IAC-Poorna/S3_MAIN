980bf3b10cb4567d1aaea57d3474fe6cc7760205
poornatest457 <98936171+poornatest457@users.noreply.github.com>
2022-03-01 19:51:50 +0530

    Private bucket access
    Private bucket access

---
+++ s3_publicaccess.tf
@@ -0,0 +1,11 @@
+resource "aws_s3_bucket" "publicaccess" {
+  bucket = "examplebuckettftest"
+  acl    = "private"
+  versioning {
+    enabled = true
+  }
+  logging {
+    target_bucket = aws_s3_bucket.log_bucket.id
+    target_prefix = "log/"
+    }
+}
